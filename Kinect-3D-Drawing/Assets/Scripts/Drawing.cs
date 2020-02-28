c5836a6d844a3df4a3977a23c979ae68f271b467
Samantha Garcia <sgarcia22@ufl.edu>
2020-02-27 20:59:40 -0500

    Reading Hand States Framework
    TODO
    -Implement 3D Drawing with Line Renderer

---
+++ Kinect-3D-Drawing/Assets/Scripts/Drawing.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kinect = Windows.Kinect;
+
+/// <summary>
+///  Possible values for Hand States
+/// </summary>
+public class HandStates
+{
+    public string Closed = "Closed";     //Fist
+    public string Open = "Open";       //Palm Open
+    public string Lasso = "Lasso";       //Pointing
+}
+
+/// <summary>
+/// Kinect 3D Drawing Script
+/// </summary>
+public class Drawing : MonoBehaviour
+{
+
+    public BodySourceView bodyView;
+    private LineRenderer lr;
+    private HandStates states;
+
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+        states = new HandStates();
+    }
+
+    void Update()
+    {
+        foreach (KeyValuePair<ulong, BodySourceView.BodyValue> body in bodyView.GetBodyGameObject())
+        {
+            if (body.Value.body.HandLeftState.ToString().Equals(states.Lasso.ToString())) Draw (body, true);
+            if (body.Value.body.HandRightState.ToString().Equals(states.Lasso.ToString())) Draw(body, false);
+        }
+    }
+
+    /// <summary>
+    /// Draw with Line Renderer
+    /// </summary>
+    /// <param name="body">Represents the Kinect Body Data</param>
+    private void Draw(KeyValuePair<ulong, BodySourceView.BodyValue> body, bool leftHand)
+    {
+        Transform handLocation;
+        //TODO
+        //if (leftHand) body.Value.obj.
+    }
+}
