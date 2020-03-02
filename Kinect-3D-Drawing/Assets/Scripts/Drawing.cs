b24dcb68df13958bd9796712f010e0e211ec6b86
Samantha Garcia <sgarcia22@ufl.edu>
2020-03-02 14:43:30 -0500

    Refactored Code
    GameManager CS, handles identifying gesture and then calls script relating to gesture

--- Kinect-3D-Drawing/Assets/Scripts/Drawing.cs
+++ Kinect-3D-Drawing/Assets/Scripts/Drawing.cs
@@ -4,30 +4,16 @@ using UnityEngine;
 using Kinect = Windows.Kinect;
 
 /// <summary>
-///  Possible values for Hand States
-/// </summary>
-public class HandStates
-{
-    public string Closed = "Closed";     //Fist
-    public string Open = "Open";       //Palm Open
-    public string Lasso = "Lasso";       //Pointing
-}
-
-/// <summary>
 /// Kinect 3D Drawing Script
-/// TODO:
-///     -Draw spheres wherever hand tip joint goes 
-///         -If too bad, then can use palm joint and approximate where finger is
-///     -Connect spheres with Line Renderer
 /// </summary>
 public class Drawing : MonoBehaviour
 {
-
+    public bool drawing;
     public BodySourceView bodyView;
+    public GameObject sphere, parent;
+
     private LineRenderer lr;
     private HandStates states;
-
-    public GameObject sphere, parent;
     private List<GameObject> spheres;
     private int index;
 
@@ -43,35 +29,25 @@ public class Drawing : MonoBehaviour
         spheres = new List<GameObject>();
         lr.positionCount = 1;
         index = 0;
+        drawing = false;
     }
 
-    void Update()
-    {
-        foreach (KeyValuePair<ulong, BodySourceView.BodyValue> body in bodyView.GetBodyGameObject())
-        {
-            //if (body.Value.body.HandLeftState.ToString().Equals(states.Lasso.ToString())) Draw (body, true);
-            //if (body.Value.body.HandRightState.ToString().Equals(states.Lasso.ToString())) Draw(body, false);
-            Kinect.Body b = body.Value.body;
-            Kinect.Joint sourceJoint = b.Joints[Kinect.JointType.HandRight];
-            GameObject temp = Instantiate(sphere, bodyView.GetVector3FromJoint(sourceJoint), Quaternion.identity);
-            temp.transform.parent = parent.transform;
-            spheres.Add(temp);
-            if (spheres.Capacity >= 2)
-            {
-                lr.positionCount += 1;
-                lr.SetPosition(index, spheres[index].transform.position);
-                lr.SetPosition(++index, temp.transform.position);
-            }
-        }
-    }
-    
     /// <summary>
     /// Draw with Line Renderer
+    /// Currently Right Hand Only
     /// </summary>
     /// <param name="body">Represents the Kinect Body Data</param>
-    private void Draw(KeyValuePair<ulong, BodySourceView.BodyValue> body, bool leftHand)
+    public void Draw(Kinect.Body b)
     {
-        Transform handLocation;
-        
+        Kinect.Joint sourceJoint = b.Joints[Kinect.JointType.HandRight];
+        GameObject temp = Instantiate(sphere, bodyView.GetVector3FromJoint(sourceJoint), Quaternion.identity);
+        temp.transform.parent = parent.transform;
+        spheres.Add(temp);
+        if (spheres.Capacity >= 2)
+        {
+            lr.positionCount += 1;
+            lr.SetPosition(index, spheres[index].transform.position);
+            lr.SetPosition(++index, temp.transform.position);
+        }
     }
 }
---
+++ Kinect-3D-Drawing/Assets/Scripts/GameManager.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Kinect = Windows.Kinect;
+
+/// <summary>
+///  Possible values for Hand States
+/// </summary>
+public class HandStates
+{
+    public string Closed = "Closed";     //Fist
+    public string Open = "Open";       //Palm Open
+    public string Lasso = "Lasso";       //Pointing
+}
+
+/// <summary>
+/// Represents different gesture states
+/// </summary>
+public enum ProcessState
+{
+    Neutral,
+    Drawing,
+    Erasing,
+    Zooming,
+    Rotating
+}
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Drawing draw;
+    [SerializeField]
+    private Erase erase;
+    // Add other classes
+
+    [SerializeField]
+    private float threshold;
+
+    private ProcessState CurrentState { get; set; }
+    public BodySourceView bodyView;
+
+    private LinkedList<string> handStates;     //Keep track of past 10 frames of hand states
+    private Dictionary<string, int> handCount; //Keep track of amount of hand states
+
+    void Awake()
+    {
+        Neutral(); // Set initial gesture to neutral
+        draw.bodyView = bodyView;
+        handStates = new LinkedList<string>();
+        handCount = new Dictionary<string, int>();
+    }
+     
+    void Update()
+    {
+        //Currently only doing Right Hand
+        foreach (KeyValuePair<ulong, BodySourceView.BodyValue> body in bodyView.GetBodyGameObject())
+        {
+            Kinect.Body b = body.Value.body;
+            string rightHandState = b.HandRightState.ToString();
+            if (rightHandState == "Unknown" || rightHandState == "NotTracked") rightHandState = "Neutral";
+
+            if (handStates.Count > 10)
+            {
+                handCount[handStates.First.Value]--;
+                handStates.RemoveFirst();
+            }
+
+            if (!handCount.ContainsKey(rightHandState)) handCount.Add(rightHandState, 0);
+            handStates.AddLast(rightHandState);
+            handCount[rightHandState]++;
+            
+            DetermineGesture(body.Value.body);
+        }
+    }
+
+    private void DetermineGesture(Kinect.Body body)
+    {
+        Tuple<string, int> max = MaxOccurrence();
+        if (max.Item2 >= (threshold / 100) * handStates.Count)
+        {
+            CurrentState = GetState(max.Item1);
+            CallClass(body);
+        }
+    }
+
+    /// <summary>
+    /// Determine maximum occurance
+    /// </summary>
+    /// <returns>Maximum occurance of hand state in Dictionary</returns>
+    private Tuple<string, int> MaxOccurrence()
+    {
+        string maxState = "";
+        int maxCount = 0;
+
+        foreach (KeyValuePair<string, int> entry in handCount)
+        {
+            if (entry.Value > maxCount)
+            {
+                maxState = entry.Key;
+                maxCount = entry.Value;
+            }
+        }
+
+        return Tuple.Create(maxState, maxCount);
+    }
+
+    /// <summary>
+    /// Change the state of the FSM
+    /// </summary>
+    /// <param name="state">Current State</param>
+    private ProcessState GetState(string state)
+    {
+        switch (state)
+        {
+            case "Closed":
+                return Neutral();
+            case "Lasso":
+                return Draw();
+            case "Opened":
+                return Erase();
+            case "Zooming":
+                return Zoom();
+            case "Rotating":
+                return Rotate();
+            default:
+                return Neutral();
+        }
+    }
+
+    /// <summary>
+    /// Call the appropriate class based on current gesture state
+    /// </summary>
+    /// <param name="body">Kinect Body</param>
+    private void CallClass (Kinect.Body body)
+    {
+        switch (CurrentState)
+        {
+            case ProcessState.Neutral:
+                break;
+            case ProcessState.Drawing:
+                draw.Draw(body);
+                break;
+            case ProcessState.Erasing:
+                break;
+            case ProcessState.Zooming:
+                break;
+            case ProcessState.Rotating:
+                break;
+        }
+    }
+
+    private ProcessState Neutral() { return ProcessState.Neutral; }
+    private ProcessState Draw() { return ProcessState.Drawing; }
+    private ProcessState Erase() { return ProcessState.Erasing; }
+    private ProcessState Zoom() { return ProcessState.Zooming; }
+    private ProcessState Rotate() { return ProcessState.Rotating; }
+}
